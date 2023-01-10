f34b3b492f6401efe724ed436b4a06faf8e8f31e
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-10 17:27:36 +0300

    Completed lab

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
+//ColorList component
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,15 +14,13 @@ function ColorList() {
     "hotpink",
   ];
 
+  const iterateThroughColors = colors.map(color=> <ColorItem key={color} color={color}/>)
+
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {iterateThroughColors}
       </ol>
     </div>
   );
